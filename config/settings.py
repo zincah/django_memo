b1084fedeca852714a71125d68329ffc886cd993
zincah <leeah9737@naver.com>
2021-11-07 00:45:50 +0900

    modify commit2

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+x0lu-9299@5%$sqx)b4)akeb1jthjla2pp4246i!)%oaov0ys'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
